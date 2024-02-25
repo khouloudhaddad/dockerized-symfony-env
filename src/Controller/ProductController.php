4e90cbaaa8ed57b0df387673d9748ff2b1e80ec3
khouloud Haddad Amamou <khouloud.haddad@gmail.com>
2024-02-25 15:50:26 +0100

    Manage DB --fix import

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -7,6 +7,7 @@ use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
 
 class ProductController extends AbstractController
 {
